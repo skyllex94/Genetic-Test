dca62c3b93b86a1356a3d8e8608643fe8a0a7f8d
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2022-05-10 13:07:15 -0400

    js dragging development

--- js/app.js
+++ js/app.js
@@ -33,3 +33,19 @@ function validateForm() {
     }
     document.querySelector('.status').innerHTML = "Изпращане...";
 }
+
+//  ---- Draggable objects ----
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
